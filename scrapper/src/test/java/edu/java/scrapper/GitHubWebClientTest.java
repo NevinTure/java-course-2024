f449929073030857d16929414562a0c18f8b356c
NevinTure <nikitastar18.b@yandex.ru>
2024-02-17 23:24:51 +0300

    add solution for hw2

---
+++ scrapper/src/test/java/edu/java/scrapper/GitHubWebClientTest.java
@@ -0,0 +1,47 @@
+package edu.java.scrapper;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import edu.java.clients.GitHubClient;
+import java.time.OffsetDateTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest("app.git-base-url=http://localhost:8080")
+@WireMockTest(httpPort = 8080)
+public class GitHubWebClientTest {
+
+    private final GitHubClient gitHubClient;
+
+    @Autowired
+    public GitHubWebClientTest(GitHubClient gitHubClient) {
+        this.gitHubClient = gitHubClient;
+    }
+
+    @Test
+    public void testGitClientWhenValidURI() {
+        //given
+        String uri = "/test_user/test_repo";
+        OffsetDateTime expectedResult = OffsetDateTime.parse("2011-01-26T19:14:43Z");
+
+        //when
+        stubFor(get(uri)
+            .willReturn(okJson("{ \"updated_at\": \"2011-01-26T19:14:43Z\" }")));
+
+        //then
+        assertThat(gitHubClient.getUpdateInfo(uri).getDateTime()).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void testGitClientWhenInvalidURI() {
+        //given
+        String uri = "invalid";
+
+        //then
+        assertThat(gitHubClient.getUpdateInfo(uri).getDateTime()).isNull();
+    }
+}
